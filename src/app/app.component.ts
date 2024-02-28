9c84bbf8478fd7c1d83a5fd7b1caa129465cb175
DylCru <dylancruickshank0@gmail.com>
2024-02-28 12:02:51 +0000

    remove routing import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { PlayerComponent } from './player/player.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlayerComponent],
+  imports: [PlayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
