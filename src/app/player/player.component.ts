0d644ce652015eea9dd9e210b0b1daa9e59a01d3
dylcru <dylancruickshank0@gmail.com>
2024-03-05 21:12:45 +0000

    remove data methods

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -2,79 +2,44 @@ import { Component } from '@angular/core';
 import { PlayerDataService } from './player-data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { NgFor, NgIf } from '@angular/common';
-import type { guildPlayer } from './guildPlayer';
-import { playerProfiles } from './playerProfiles';
+import { playerProfiles } from '../playerProfiles';
 import copy from 'copy-to-clipboard';
 import { NgbProgressbarModule, NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { TestData } from './TestData';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-player',
   standalone: true,
-  imports: [HttpClientModule, NgIf, NgFor, NgbTooltip, NgbProgressbarModule],
+  imports: [HttpClientModule, NgIf, NgFor, NgbTooltip, NgbProgressbarModule, FormsModule],
   templateUrl: './player.component.html',
   styleUrl: './player.component.css'
 })
 
 export class PlayerComponent {
   data: any[]
-  players: guildPlayer[]
   skyblockPlayers: playerProfiles[]
   belowReq: playerProfiles[]
   loaded: boolean
   guildSize: number = 0
-  staffRole: String
-  kickReason: String = "";
+  staffRole: string
+  kickReason: string = "";
+  levelReq: number = 0;
 
   constructor(private playerDataService: PlayerDataService) {
-    this.players = []
     this.data = []
     this.skyblockPlayers = []
     this.belowReq = []
     this.loaded = false
     this.staffRole = 'Staff'
 
-    //this.belowReq = TestData  
+    this.belowReq = TestData  
   }
 
-   ngOnInit() {
-    this.getData()
-  }
+  ngOnInit() {}
 
-  getData() {
-    this.playerDataService.getGuild().subscribe(res => {
-      this.data = res.guild['members']
-      this.guildSize = res.guild['members'].length
-      this.loadPlayers()
-    })
-  }
-
-  loadPlayers() {
-    console.log(this.data)
-    for (const p of this.data) {
-      this.players.push({
-        uuid: p.uuid,
-        rank: p.rank
-      })
-    }
-  }
-
-  getPlayerData() {
-    for (let p of this.players) {
-      let name: string
-      this.playerDataService.getPlayerName(p.uuid).subscribe(res => {
-        name = res.username
-        this.playerDataService.getSkyblockData(p.uuid).subscribe(data => {
-          this.skyblockPlayers.push({
-            uuid: p.uuid,
-            name: name,
-            experience: this.getPlayerProfileLevels(data.profiles, p, name),
-            highestLevel: Math.max(...this.getPlayerProfileLevels(data.profiles, p, name)),
-            guildRank: p.rank
-          })
-        })
-      })
-    }
+  loadDataFromStorage() {
+    
   }
 
   checkRequirement(profiles: playerProfiles[], levelReq: number) {
@@ -88,19 +53,8 @@ export class PlayerComponent {
     }
   }
 
-  getPlayerProfileLevels(profiles: any, p: guildPlayer, name: string): number[] {
-    let xp: number[] = []
-    for (let i of profiles) {
-      try {
-        xp.push(i.members[p.uuid].leveling.experience)
-      } catch {
-        console.log(`ERROR: Couldnt push profile for ${name} (api probably off): `, i)
-      }
-    }
-    return xp
-  }
-
-  copyKickCommand(name: string, kickReason: string) {
-    copy(`/g kick ${name} ${kickReason}`)
+  copyKickCommand(name: string) {
+    console.log(this.kickReason)
+    copy(`/g kick ${name} ${this.kickReason}`)
   }
 }
