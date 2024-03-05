289b362709b3bd89359aa58e8e2bbec28c33a6c8
dylcru <dylancruickshank0@gmail.com>
2024-03-05 21:06:28 +0000

    add promotion checker component

---
+++ src/app/promotion-checker/promotion-checker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promotion-checker',
+  standalone: true,
+  imports: [],
+  templateUrl: './promotion-checker.component.html',
+  styleUrl: './promotion-checker.component.css'
+})
+export class PromotionCheckerComponent {
+
+}
