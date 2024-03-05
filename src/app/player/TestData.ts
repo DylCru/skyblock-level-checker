903834cb89697c5cd765568d23cbaae43f47efd4
dylcru <dylancruickshank0@gmail.com>
2024-03-05 10:47:52 +0000

    move test data to own file

---
+++ src/app/player/TestData.ts
@@ -0,0 +1,71 @@
+import { playerProfiles } from "./playerProfiles";
+
+export const TestData: playerProfiles[] = [
+    {
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Nightwatch'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Insomniac'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Insomniac'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Insomniac'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Insomniac'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Insomniac'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Staff'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Staff'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Insomniac'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Sleeper'
+      },{
+        uuid: 'bf4fba4359bb44ffb58f37c6b4940333',
+        name: 'Rain_stxrm',
+        experience: [23213, 4324, 13213, 5322],
+        highestLevel: 23213,
+        guildRank: 'Insomniac'
+      }
+    ]   
