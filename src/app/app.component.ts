bf909d4e99fd943ab9feaefc38f6720174d32b0a
DylCru <dylancruickshank0@gmail.com>
2024-03-06 17:22:12 +0000

    move functionality to homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,111 +2,32 @@ import { Component } from '@angular/core';
 import { PlayerComponent } from './player/player.component';
 import { FormsModule } from '@angular/forms';
 import { PromotionCheckerComponent } from './promotion-checker/promotion-checker.component';
-import { PlayerDataService } from './player/player-data.service';
-import { guildPlayer } from './guildPlayer';
-import { playerProfiles } from './playerProfiles';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationStart, Router, RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PlayerComponent, FormsModule, PromotionCheckerComponent],
+  imports: [PlayerComponent, FormsModule, PromotionCheckerComponent, NgbAlert, RouterLink, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Skyblock Level Checker';
-  checkRequirements = true
-  guildData: any[] = []
-  guildSize: number = 0;
-  players: guildPlayer[] = []
-  skyblockPlayers: playerProfiles[] = []
+  route = '/'
 
-  constructor(private playerDataService: PlayerDataService) {
-  }
-
-  ngOnInit() {
-    this.getData()
-  }
-
-  getData() {
-    this.playerDataService.getGuild().subscribe(res => {
-      this.guildData = res.guild['members']
-      this.guildSize = res.guild['members'].length
-      this.loadPlayers()
+  constructor(private router: Router) {
+    router.events.subscribe(route => {
+      if (route instanceof NavigationStart) {
+        if (route.url === '/') {
+          this.route = 'home'
+        } else {
+          this.route = route.url.split('/')[1]
+        }
+        console.log(this.route)
+      }
     })
   }
 
-  loadPlayers() {
-    console.log(this.guildData)
-    for (const p of this.guildData) {
-      this.players.push({
-        uuid: p.uuid,
-        rank: p.rank
-      })
-    }
-  }
-
-  getPlayerData() {
-    for (let p of this.players) {
-      let name: string
-      this.playerDataService.getPlayerName(p.uuid).subscribe(res => {
-        name = res.username
-        this.playerDataService.getSkyblockData(p.uuid).subscribe(data => {
-          this.skyblockPlayers.push({
-            uuid: p.uuid,
-            name: name,
-            experience: this.getPlayerProfileLevels(data.profiles, p, name),
-            highestLevel: Math.max(...this.getPlayerProfileLevels(data.profiles, p, name)),
-            guildRank: p.rank
-          })
-          this.loadIntoLocalStorage(p.uuid)
-        })
-      })
-    }
-  }
-
-  getPlayerProfileLevels(profiles: any, p: guildPlayer, name: string): number[] {
-    let xp: number[] = []
-    for (let i of profiles) {
-      try {
-        xp.push(i.members[p.uuid].leveling.experience)
-      } catch {
-        console.log(`ERROR: Couldnt push profile for ${name} (api probably off): `, i)
-      }
-    }
-    return xp
-  }
-
-  loadIntoLocalStorage(uuid: string) {
-    for (let i of this.skyblockPlayers) {
-      localStorage.setItem(uuid, JSON.stringify(i))
-    }
-  }
-
-  loadTime() {
-    const time = new Date().toISOString()
-
-    localStorage.setItem('timestamp', time)
-  }
-
-  getTimeStamp() {
-    return localStorage.getItem('timestamp')
-  }
-
-  getPlayerProfileFromStorage() {
-    for (let i of this.players) {
-      const player = localStorage.getItem(i.uuid)
-      
-    }
-  }
-
-  logLocalStorage() {
-    for (let i = 0; i < localStorage.length; i++) {
-      const key = localStorage.key(i);
-      if (key) {
-        const value = localStorage.getItem(key);
-        console.log(`Key: ${key}, Value: ${value}`);
-      }
-    }
-  }
 }
