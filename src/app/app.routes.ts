ab34e29ec6538038f06fc38e653731c8addb2db5
dylcru <dylancruickshank0@gmail.com>
2024-03-12 15:22:11 +0000

    add route to promotion page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PlayerComponent } from './player/player.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PromotionCheckerComponent } from './promotion-checker/promotion-checker.component';
 
 export const routes: Routes = [
     {path: '', component: HomepageComponent},
-    {path: 'requirements', component: PlayerComponent}
+    {path: 'requirements', component: PlayerComponent},
+    {path: 'promotion', component: PromotionCheckerComponent}
 ];
